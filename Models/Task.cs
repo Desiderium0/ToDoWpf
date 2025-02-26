8cddbd4cbad9ab9833a13e1bfc4a6eadada3984e
Desiderium0 <black.cat2222@mail.ru>
2025-02-26 12:04:15 +0700

    WPF

---
+++ Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoWPF.Models
+{
+    internal class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public bool IsCompleted { get; set; }
+        public List<Task> Tasks { get; set; }
+    }
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using ToDoWPF.Infrastructure.Commands;
+using ToDoWPF.ViewModels.Base;
+
+namespace ToDoWPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Properties
+        private string? _Title;
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private List<string> _Tasks;
+
+        public List<string> Tasks
+        {
+            get => _Tasks;
+            set => Set(ref _Tasks, value);
+        }
+        #endregion
+
+        #region Commands
+        #region CloseApplicationCommandExecuted
+        public ICommand CloseApplicationCommand { get; }
+        private bool CanCloseApplicationCommandExecuted(object p) => true;
+        private void OnCloseApplicationCommandExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #region MinimizeApplicationCommand
+        public ICommand MinimizeApplicationCommand { get; }
+        private bool CanMinimizeApplicationCommandExecuted(object p) => true;
+        private void OnMinimizeApplicationCommandExecuted(object p)
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        }
+        #endregion
+
+        #region MaximizeApplicationCommand
+        public ICommand MaximizeApplicationCommand { get; }
+        public WindowState CurrentWindowState 
+        {
+            get => Application.Current.MainWindow.WindowState;
+        }
+        private bool CanMaximizeApplicationCommandExecuted(object p) => true;
+        private void OnMaximizeApplicationCommandExecuted(object p)
+        {
+            if (CurrentWindowState == WindowState.Normal)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        }
+        #endregion
+
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecuted);
+            MinimizeApplicationCommand = new LambdaCommand(OnMinimizeApplicationCommandExecuted, CanMinimizeApplicationCommandExecuted);
+            MaximizeApplicationCommand = new LambdaCommand(OnMaximizeApplicationCommandExecuted, CanMaximizeApplicationCommandExecuted);
+
+            #endregion
+        }
+    }
+}
+    
